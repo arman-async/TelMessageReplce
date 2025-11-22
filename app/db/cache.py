12d46bd25445f982260de6509c3e039814c0f3da
Alireza Moussavian <armanparsi206@outlook.com>
2025-11-22 19:16:09 +0330

    reollback to sync redis

--- app/db/cache.py
+++ app/db/cache.py
@@ -4,7 +4,7 @@ import pickle
 from functools import wraps
 from typing import Any
 
-from redis.asyncio import Redis
+from redis import Redis
 
 
 class RedisCacheFunction:
@@ -12,16 +12,18 @@ class RedisCacheFunction:
         self.redis_client = redis_client
 
     def gen_key(self, *args, **kwargs) -> str:
-        return base64.b64encode(pickle.dumps({"args": args, "kwargs": kwargs})).decode()
+        return base64.b64encode(
+            pickle.dumps({"args": args, "kwargs": kwargs})
+        ).decode()
 
-    async def get_cache(self, key: str) -> Any:
-        cached_data = await self.redis_client.get(key)
+    def get_cache(self, key: str) -> Any:
+        cached_data = self.redis_client.get(key)
         if isinstance(cached_data, bytes):
             return pickle.loads(cached_data)
         return None
 
-    async def set_cache(self, key: str, data: Any, expire: int | None = None):
-        await self.redis_client.set(key, pickle.dumps(data), ex=expire)
+    def set_cache(self, key: str, data: Any, expire: int | None = None):
+        self.redis_client.set(key, pickle.dumps(data), ex=expire)
 
     def filter_inputs(
         self,
@@ -38,7 +40,7 @@ class RedisCacheFunction:
     def cache_async(
         self,
         expire: int | None = None,
-        exclude: set[str] | None = None,
+        exclode: set[str] | None = None,
         include: set[str] | None = None,
     ):
         def decorator(func):
@@ -49,16 +51,15 @@ class RedisCacheFunction:
                 bound = sig.bind(*args, **kwargs)
                 bound.apply_defaults()
                 params_dict: dict[str, Any] = dict(bound.arguments)
-                params = self.filter_inputs(params_dict, exclude, include)
+                params = self.filter_inputs(params_dict, exclode, include)
                 key = self.gen_key(params)
-
-                # now properly awaited
-                cached_value = await self.get_cache(key)
-                if cached_value is not None:
-                    return cached_value
+                is_cache = self.get_cache(key)
+                if is_cache:
+                    return is_cache
 
                 response = await func(*args, **kwargs)
-                await self.set_cache(key, response, expire)
+                self.set_cache(key, response, expire)
+
                 return response
 
             return wrapper
