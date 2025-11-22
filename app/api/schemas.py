df5e7dc2d49a3234bface9dfd53d88ef443c0348
Alireza Moussavian <armanparsi206@outlook.com>
2025-11-22 19:14:30 +0330

    update schemas

--- app/api/schemas.py
+++ app/api/schemas.py
@@ -25,6 +25,11 @@ class MessageActionsCreate(BaseModel):
     regex: str
     acction: enums.MessageActions
     message_replace: str
+    entities: str
+    inline_keyboard_json: str
+    max_total_uses: int
+    max_uses_per_user: int
+    run_after_join_check: bool
 
 
 class MessageActionsUpdate(MessageActionsCreate):
