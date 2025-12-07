db131edc51d4b3132751c87d45d402d97522b28e
Alireza Moussavian <armanparsi206@outlook.com>
2025-12-07 12:50:39 +0330

    fix MessageActionsCreate create

--- app/api/schemas.py
+++ app/api/schemas.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+import json
+
+from pydantic import BaseModel, field_validator
 
 from app.db import enums
 
@@ -21,15 +23,44 @@ class ForcedSubscriptionRead(ForcedSubscriptionCreate):
 
 
 class MessageActionsCreate(BaseModel):
+    class Entities(BaseModel):
+        type: str
+        offset: int
+        length: int
+        url: str | None = None
+        anguage:str | None = None
+        custom_emoji_id:int| None = None
+
+    class InlineKeyboardJson(BaseModel):
+        class InlineKeyboard(BaseModel):
+            text: str
+            url: str
+
+        inline_keyboard: list[list[InlineKeyboard]]
+
     name: str
     regex: str
-    acction: enums.MessageActions
+    action: enums.MessageActions
     message_replace: str
-    entities: str
-    inline_keyboard_json: str
+    entities: list[Entities|None] = []
+    inline_keyboard_json: InlineKeyboardJson | None
     max_total_uses: int
     max_uses_per_user: int
     run_after_join_check: bool
+
+    @field_validator("entities", "inline_keyboard_json")
+    def convert_dict_to_json(cls, v):
+        if v is None:
+            return json.dumps(dict())
+        
+        if isinstance(v, BaseModel):
+            v = v.model_dump()
+
+        if isinstance(v, list):
+            v = [
+                item.model_dump() if isinstance(item, BaseModel) else item for item in v
+            ]
+        return json.dumps(v)
 
 
 class MessageActionsUpdate(MessageActionsCreate):
